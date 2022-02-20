f7ec84b1fd7270b1161c2660ca5b83e982bf670b
rainier-dev-88 <rainier.dev.88@gmail.com>
2022-02-20 16:30:22 +0800

    update pipeline.

--- build-linux.sh
+++ build-linux.sh
@@ -1,5 +1,5 @@
 rm -rf dist
 
-dotnet build ./Sample/TelstraPurple.ConsoleSample/TelstraPurple.ConsoleSample.csproj -r linux-x64 --sc -c Release -o ./dist/TelstraPurple.ConsoleSample
+dotnet build ./Sample/TelstraPurple.ConsoleSample/TelstraPurple.ConsoleSample.csproj -r linux-x64 -c Release -o ./dist/TelstraPurple.ConsoleSample
 
-dotnet build ./Sample/TelstraPurple.ConsoleFactorySample/TelstraPurple.ConsoleFactorySample.csproj -r linux-x64 --sc -c Release -o ./dist/TelstraPurple.ConsoleFactorySample
+dotnet build ./Sample/TelstraPurple.ConsoleFactorySample/TelstraPurple.ConsoleFactorySample.csproj -r linux-x64 -c Release -o ./dist/TelstraPurple.ConsoleFactorySample
