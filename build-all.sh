895d068bd1d7cdc6a3d6750a0d3a7dbf11c6e3eb
rainier-dev-88 <rainier.dev.88@gmail.com>
2022-02-20 20:18:31 +0800

    add git instruction for cloning. remove unused shell

--- build-all.sh
+++
@@ -1,9 +0,0 @@
-rm -rf dist
-
-dotnet build ./Sample/TelstraPurple.ConsoleSample/TelstraPurple.ConsoleSample.csproj -r win-x64 -c Release -o ./dist/windows/TelstraPurple.ConsoleSample
-
-dotnet build ./Sample/TelstraPurple.ConsoleFactorySample/TelstraPurple.ConsoleFactorySample.csproj -r win-x64 -c Release -o ./dist/windows/TelstraPurple.ConsoleFactorySample
-
-dotnet build ./Sample/TelstraPurple.ConsoleSample/TelstraPurple.ConsoleSample.csproj -r linux-x64 -c Release -o ./dist/linux/TelstraPurple.ConsoleSample
-
-dotnet build ./Sample/TelstraPurple.ConsoleFactorySample/TelstraPurple.ConsoleFactorySample.csproj -r linux-x64 -c Release -o ./dist/linux/TelstraPurple.ConsoleFactorySample
--- readme.md
+++ readme.md
@@ -8,7 +8,7 @@ A small library design to control a bot that moves in a 2 dimensional space.
 #### BEFORE YOU START ####
 * Download and install [.NET6 SDK](https://dotnet.microsoft.com/en-us/download/dotnet/6.0 ".NET6 SDK").
 * Download git for [Windows](https://gitforwindows.org/ "Windows")/[Linux](https://git-scm.com/download/linux "Linux")
-
+* Clone repository `git clone https://github.com/rainier-dev-88/telsbot.git`
 - - - -
 #### DOCUMENTATION ####
 
